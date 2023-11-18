648b86daacdcbf1aff279c1fe53ba27230bf799a
Yuuki Rika <yvvki@duck.com>
2023-11-19 02:08:56 +0700

    🧹 Refactor
    Rename variable to its program name

--- vsc2tm.js
+++ vsc2tm.js
@@ -1,50 +1,62 @@
-import json5 from 'json5';
-import plist from 'plist';
-import fs from 'fs';
+import fs from 'fs/promises';
 
-function convert(vscTheme) {
-    const tmTheme = {
-        name: vscTheme.name,
-        settings: vscTheme.tokenColors
+import JSON5 from 'json5';
+import plist from 'plist';
+
+/**
+ * Covert a VSCode into TextMate theme configuration
+ * @param {Object} vscode
+ * @param {string} vscode.name
+ * @param {Object<string, string>} vscode.colors
+ * @param {Object[]} vscode.tokenColors
+ * @param {String[]} vscode.tokenColors.scope
+ * @param {Object} vscode.tokenColors.settings
+ */
+export function convert(vscode) {
+    const textmate = {
+        name: vscode.name,
+        settings: vscode.tokenColors,
     }
 
-    const defaultSettings = tmTheme.settings.find(setting => !setting.scope);
+    const defaultSettings = textmate.settings.find(setting => !setting.scope);
 
     if (!defaultSettings) {
-        tmTheme.settings.unshift({ settings: {}});
+        textmate.settings.unshift({ settings: {} });
     }
 
-    const tmThemeDefaultSettings = tmTheme.settings[0].settings;
-    const vscThemeColors = vscTheme.colors;
+    const textmateDefaultSettings = textmate.settings[0].settings;
+    const vscodeColors = vscode.colors;
 
-    const mapper = new SettingsMapper({tmThemeDefaultSettings, vscThemeColors});
+    const mapper = new SettingsMapper({ tmThemeDefaultSettings: textmateDefaultSettings, vscThemeColors: vscodeColors });
     mapper.addSetting("editorCursor.foreground", "caret");
     mapper.addSetting("editor.selectionBackground", "selection");
     mapper.addSetting("editor.lineHighlightBackground", "lineHighlight");
     mapper.addSetting("editor.foreground", "foreground");
     mapper.addSetting("editor.background", "background");
     mapper.addSetting("editorWhitespace.foreground", "invisibles");
-    for (var i = 1; i < tmTheme.settings.length; i++) {
-       const scope = tmTheme.settings[i].scope
-       if (scope) {
-           tmTheme.settings[i].scope = scope.toString()
-       } 
+    for (var i = 1; i < textmate.settings.length; i++) {
+        const scope = textmate.settings[i].scope;
+        if (scope) {
+            textmate.settings[i].scope = scope.toString();
+        }
     }
-    return tmTheme
+    return textmate;
 }
+export default convert
 
 class SettingsMapper {
-    constructor({ tmThemeDefaultSettings, vscThemeColors }) {
-        this.tmThemeDefaultSettings = tmThemeDefaultSettings;
-        this.vscThemeColors = vscThemeColors;
+    constructor({ textmateDefaultSettings, vscodeColors }) {
+        this.textmateDefaultSettings = textmateDefaultSettings;
+        this.vscodeColors = vscodeColors;
     }
 
-    addSetting(fromKey, toKey) {
-        if (fromKey in this.vscThemeColors) {
-            this.tmThemeDefaultSettings[toKey] = this.vscThemeColors[fromKey]
+    addSetting(source, target) {
+        if (source in this.vscodeColors) {
+            this.textmateDefaultSettings[target] = this.vscodeColors[source];
         }
     }
 }
 
-const vscTheme = json5.parse(fs.readFileSync(process.argv[2], "utf8"))
-fs.writeFileSync(process.argv[3], plist.build(convert(vscTheme)))
+const vscode = JSON5.parse(await fs.readFile(process.argv[2], "utf8"));
+const textmate = convert(vscode);
+await fs.writeFile(process.argv[3], plist.build(textmate));
